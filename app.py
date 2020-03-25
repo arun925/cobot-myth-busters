b18e3951772fd67ed63faf1c249199cfdf70163b
S <aru@a483e795fd8b.ant.amazon.com>
2020-03-25 21:55:49 +0530

    Adding additional text messages and validation

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def bot():
     responded = False
 
     if 'Hi' in incoming_msg or 'Hey' in incoming_msg or 'Menu' in incoming_msg:
-        text = f'Hello 🙋🏽‍♂, \nThis is a Covid-whatsapp-Bot to provide latest information updates, self check on symptoms and identifying fake news .\n For any emergency 👇 \n 📞 Helpline: 011-23978046 | Toll-Free Number: 1075 \n ✉ Email: ncov2019@gov.in \n\n Please enter one of the following option 👇 \n *A*. *Fake news* Identification (Myth Busters) \n *B*. *Self-Assessment* for COVID symptoms. \n *C*. Covid-19 statistics *Worldwide*. \n *D*. Covid-19 cases in *India*.  \n *E*. How does it *Spread*?'
+        text = f'Hello 🙋🏽‍♂, \nThis is a Covid-whatsapp-Bot to provide latest information updates, self check on symptoms and identifying fake news . \n\n Please enter one of the following option 👇 \n *A*. *Fake news* Identification (Myth Busters) \n *B*. *Self-Assessment* for COVID symptoms. \n *C*. Covid-19 statistics *Worldwide*. \n *D*. Covid-19 cases in *India*.  \n *E*. How does it *Spread*?'
         msg.body(text)
         responded = True
 
@@ -26,7 +26,7 @@ def bot():
         r = requests.get('https://coronavirus-19-api.herokuapp.com/all')
         if r.status_code == 200:
             data = r.json()
-            text = f'_Covid-19 Cases Worldwide_ \n\nConfirmed Cases : *{data["cases"]}* \n\nDeaths : *{data["deaths"]}* \n\nRecovered : *{data["recovered"]}*  \n\n 👉 Type *B* to check cases in *India* \n 👉 Type *B, C, D, E, F, G* to see other options \n 👉 Type *Menu* to view the Main Menu'
+            text = f'_Covid-19 Cases Worldwide_ \n\nConfirmed Cases : *{data["cases"]}* \n\nDeaths : *{data["deaths"]}* \n\nRecovered : *{data["recovered"]}*  \n\n 👉 Type *D to check cases in *India* \n 👉 Type *A, B* to see other options \n 👉 Type *Menu* to view the Main Menu'
         else:
             text = 'Could not retrieve the results at this time, sorry.'
         msg.body(text)
@@ -56,29 +56,24 @@ def bot():
         text = 'Self Analysis coming soon.. Sorry for the inconvenience'
         msg.body(text)
         responded = True
+    if "Q" in incoming_msg:
+        text = incoming_msg
+        msg.body(text)
 
     if 'A' in incoming_msg:
-        text = f'👇 Choose any below options to get the correct news on few myths (Source WHO (Myth busters))\n *1*. Do you still believe *hot/humid* climate does not spread Corona ? ' \
-               f' \n *2*. Does *Cold weather and snow* CANNOT kill the new coronavirus. \n *3*. Taking *hot bath* to kill virus ? ' \
-               f' \n *4*. Afraid that *mosquito bites* spreads virus ?*.  \n *5*. Are *hand dryers* effective in killing the new coronavirus?. \n *6*. Can an *ultraviolet disinfection lamp* kill the new coronavirus?. ' \
-               f' \n *7*. How effective are *thermal scanners* in detecting people infected with the new coronavirus?. \n *8*. Can *spraying alcohol or chlorine* all over your body kill the new coronavirus?. ' \
-               f' \n *9*. Do *vaccines against pneumonia* protect you against the new coronavirus?. \n*10*. Can regularly *rinsing your nose with saline* help prevent infection with the new coronavirus? ' \
-               f' \n *11*. Can *eating garlic* help prevent infection with the new coronavirus? \n *12*. Does the new coronavirus *affect older people, or are younger people also susceptible?*. ' \
-               f' \n *13*. Are *antibiotics effective in preventing* and treating the new coronavirus? \n *14* Are there *any specific medicines* to prevent or treat the new coronavirus? \n '
+        text = f'👇 Choose any below options to get the correct news on few myths (Source WHO (Myth busters))\n\n *1*. Do you still believe *hot/humid* climate does not spread Corona ? ' \
+               f' \n\n *2*. Does *Cold weather and snow* CANNOT kill the new coronavirus. \n\n *3*. Taking *hot bath* to kill virus ? ' \
+               f' \n\n *4*. Afraid that *mosquito bites* spreads virus ?*.  \n\n *5*. Are *hand dryers* effective in killing the new coronavirus?. \n\n *6*. Can an *ultraviolet disinfection lamp* kill the new coronavirus?. ' \
+               f' \n\n *7*. How effective are *thermal scanners* in detecting people infected with the new coronavirus?. \n\n *8*. Can *spraying alcohol or chlorine* all over your body kill the new coronavirus?. ' \
+               f' \n\n *9*. Do *vaccines against pneumonia* protect you against the new coronavirus?. \n\n*10*. Can regularly *rinsing your nose with saline* help prevent infection with the new coronavirus? ' \
+               f' \n\n *11*. Can *eating garlic* help prevent infection with the new coronavirus? \n\n *12*. Does the new coronavirus *affect older people, or are younger people also susceptible?*. ' \
+               f' \n\n *13*. Are *antibiotics effective in preventing* and treating the new coronavirus? \n\n *14* Are there *any specific medicines* to prevent or treat the new coronavirus? '
         msg.body(text)
         responded = True
 
     if '1' in incoming_msg:
-        text = f'👇 Choose any below options to get the correct news on few myths (Source WHO (Myth busters))\n *1*. Do you still believe *hot/humid* climate does not spread Corona ? ' \
-               f' \n *2*. Does *Cold weather and snow* CANNOT kill the new coronavirus. \n *3*. Taking *hot bath* to kill virus ? ' \
-               f' \n *4*. Afraid that *mosquito bites* spreads virus ?*.  \n *5*. Are *hand dryers* effective in killing the new coronavirus?. \n *6*. Can an *ultraviolet disinfection lamp* kill the new coronavirus?. ' \
-               f' \n *7*. How effective are *thermal scanners* in detecting people infected with the new coronavirus?. \n *8*. Can *spraying alcohol or chlorine* all over your body kill the new coronavirus?. ' \
-               f' \n *9*. Do *vaccines against pneumonia* protect you against the new coronavirus?. \n*10*. Can regularly *rinsing your nose with saline* help prevent infection with the new coronavirus? ' \
-               f' \n *11*. Can *eating garlic* help prevent infection with the new coronavirus? \n *12*. Does the new coronavirus *affect older people, or are younger people also susceptible?*. ' \
-               f' \n *13*. Are *antibiotics effective in preventing* and treating the new coronavirus? \n *14* Are there *any specific medicines* to prevent or treat the new coronavirus? \n '
         msg.media(
             'https://www.who.int/images/default-source/health-topics/coronavirus/myth-busters/52.tmb-1920v.png?sfvrsn=862374e_1')
-        msg.body(text)
         responded = True
     if '2' in incoming_msg:
         msg.media(
@@ -112,23 +107,23 @@ def bot():
         msg.media(
             'https://www.who.int/images/default-source/health-topics/coronavirus/myth-busters/web-mythbusters/11.tmb-1920v.png?sfvrsn=97f2a51e_2')
         responded = True
-    if '10' in incoming_msg:
+    if '10' == incoming_msg:
         msg.media(
             'https://www.who.int/images/default-source/health-topics/coronavirus/myth-busters/23.tmb-1920v.png?sfvrsn=c65dad38_3')
         responded = True
-    if '11' in incoming_msg:
+    if '11' == incoming_msg:
         msg.media(
             'https://www.who.int/images/default-source/health-topics/coronavirus/myth-busters/19.tmb-1920v.png?sfvrsn=52adfc93_3')
         responded = True
-    if '12' in incoming_msg:
+    if '12' == incoming_msg:
         msg.media(
             'https://www.who.int/images/default-source/health-topics/coronavirus/myth-busters/mythbuster-2.tmb-1920v.png?sfvrsn=635d24e5_3')
         responded = True
-    if '13' in incoming_msg:
+    if '13' == incoming_msg:
         msg.media(
             'https://www.who.int/images/default-source/health-topics/coronavirus/myth-busters/mythbuster-3.tmb-1920v.png?sfvrsn=10657e42_3')
         responded = True
-    if '14' in incoming_msg:
+    if '14' == incoming_msg:
         msg.media(
             'https://www.who.int/images/default-source/health-topics/coronavirus/myth-busters/web-mythbusters/mythbuster-4.tmb-1920v.png?sfvrsn=e163bada_3')
         responded = True
@@ -138,13 +133,6 @@ def bot():
         msg.media(
             'https://user-images.githubusercontent.com/34777376/77290801-f2421280-6d02-11ea-8b08-fdb516af3d5a.jpeg')
         responded = True
-
-    # if 'G' in incoming_msg:
-    #     text = f'_Coronavirus infection can be prevented through the following means_ 👇 \n ✔️ Clean hand with soap and water or alcohol-based hand rub \n https://youtu.be/EJbjyo2xa2o \n\n ✔️ Cover nose and mouth when coughing & sneezing with a tissue or flexed elbow \n https://youtu.be/f2b_hgncFi4 \n\n ✔️ Avoid close contact & maintain 1-meter distance with anyone who is coughing or sneezin \n https://youtu.be/mYyNQZ6IdRk \n\n ✔️ Isolation of persons traveling from affected countries or places for at least 14 day \n https://www.mohfw.gov.in/AdditionalTravelAdvisory1homeisolation.pdf \n\n ✔️ Quarantine if advise \n https://www.mohfw.gov.in/Guidelinesforhomequarantine.pdf \n\n 👉 Type *A, B, C, D, E, F* to see other option \n 👉 Type *Menu* to view the Main Menu'
-    #     msg.body(text)
-    #     msg.media(
-    #         'https://user-images.githubusercontent.com/34777376/77290864-1c93d000-6d03-11ea-96fe-18298535d125.jpeg')
-    #     responded = True
 
     if responded == False:
         msg.body('I only know about corona virus, sorry!')
